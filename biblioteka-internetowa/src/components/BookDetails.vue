87686b65e122cc4146024a8a09b6e3f2b6c10722
jkurowski7 <jkurowski37@gmail.com>
2022-05-01 14:17:41 +0200

    copies

--- biblioteka-internetowa/src/components/BookDetails.vue
+++ biblioteka-internetowa/src/components/BookDetails.vue
@@ -3,14 +3,15 @@
     <h1>{{ bookDetails.title }} - {{ bookDetails.author }}</h1>
     <img :src="bookDetails.cover" class="cover" />
     <p>{{ bookDetails.desc }}</p>
-    <p>{{ bookDetails.copies }}</p>
+    <p>Dostępne egzemplarze:</p>
+    <div v-for="copy of bookDetails.copies">
+      <span v-if="copy">{{ copy }}</span>
+      <span v-if="!copy">Dostępny</span>
+    </div>
   </div>
 </template>
 
 <script>
-  db.collection("books").doc("JJUY8MBvddfmAuITxVNC").collection('copies').get().then((copy) => {
-    console.log(copy.data())
-  })
 export default {
   props: ["bookId"],
   data() {
@@ -24,13 +25,22 @@ export default {
         this.bookDetails["author"] = doc.data().author;
         this.bookDetails["desc"] = doc.data().desc;
         this.bookDetails["cover"] = doc.data().cover;
-        this.bookDetails["copies"] = doc.data().copies;
+        this.bookDetails["copies"] = []
+        var copies = doc.data().copies
+        for(let i = 0; i < copies.length; i++){ 
+          if (copies[i]) {
+            var realDate = new Date(copies[i].seconds * 1000)
+            var date = realDate.getDate() + '/' + realDate.getMonth() + '/' + realDate.getFullYear()
+            this.bookDetails["copies"].push(date)
+          } else {
+            this.bookDetails["copies"].push(null)
+          }
+        }
+        this.bookDetails["copies"].sort()
       });
   },
 };
-
 </script>
-
 
 <style>
   img.cover {
