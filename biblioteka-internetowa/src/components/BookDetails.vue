ca77459160c6b422ed51275f5c9db8851e1da213
jkurowski7 <jkurowski37@gmail.com>
2022-05-06 02:36:55 +0200

    details

--- biblioteka-internetowa/src/components/BookDetails.vue
+++ biblioteka-internetowa/src/components/BookDetails.vue
@@ -12,29 +12,43 @@
           <h1 v-else>
           {{ bookDetails.title }}
           </h1>
-          <h3>Autor: {{ bookDetails.author }}</h3><hr>
+          <h3>{{ bookDetails.author }}</h3><hr>
           <span>Opis</span>
           <p>{{ bookDetails.desc }}</p><hr>
-          <div class="row">
-            <div class="col-4">
-              <ul style="list-style: none;">
-                <li>Rok wydania: </li><hr>
-                <li>Liczba stron: </li><hr>
-                <li>Kategorie: </li><hr>
-                <li>Wydawca: </li><hr>
-                <li>Numer ISBN:</li><hr>
-              </ul>
-            </div>
-            <div class="col-8">
-              <ul style="list-style: none;">
-                <li>{{ bookDetails.release_year }}</li><hr>
-                <li>{{ bookDetails.page_count }}</li><hr>
-                <li>{{ bookDetails.genre }}</li><hr>
-                <li>{{ bookDetails.publishing }}</li><hr>
-                <li>{{ bookDetails.isbn }}</li><hr>
-              </ul>
-            </div>
-          </div>
+          <table>
+            <tr v-if="bookDetails.original_title">
+              <th>Tytuł oryginału: </th>
+              <th>{{ bookDetails.original_title }}</th>
+            </tr>
+            <tr v-if="bookDetails.series">
+              <th>Cykl: </th>
+              <th>{{ bookDetails.series }} (tom {{ bookDetails.volume }})</th>
+            </tr>
+            <tr v-if="bookDetails.release_year">
+              <th>Rok wydania: </th>
+              <th>{{ bookDetails.release_year }}</th>
+            </tr>
+            <tr v-if="bookDetails.page_count">
+              <th>Liczba stron: </th>
+              <th>{{ bookDetails.page_count }}</th>
+            </tr>
+            <tr v-if="bookDetails.publishing">
+              <th>Wydawca: </th>
+              <th>{{ bookDetails.publishing }}</th>
+            </tr>
+            <tr v-if="bookDetails.release_year">
+              <th>Rok wydania: </th>
+              <th>{{ bookDetails.release_year }}</th>
+            </tr>
+            <tr v-if="bookDetails.isbn">
+              <th>ISBN: </th>
+              <th>{{ bookDetails.isbn }}</th>
+            </tr>
+            <tr v-if="bookDetails.translation">
+              <th>Przekład: </th>
+              <th>{{ bookDetails.translation }}</th>
+            </tr>
+          </table>
         </div> 
       </div>
       <p>Dostępne egzemplarze:</p>
@@ -67,6 +81,8 @@ export default {
         this.bookDetails["publishing"] = doc.data().publishing;
         this.bookDetails["genre"] = doc.data().genre;
         this.bookDetails["cover"] = doc.data().cover;
+        this.bookDetails["original_title"] = doc.data().original_title;
+        this.bookDetails["translation"] = doc.data().translation;
         this.bookDetails["copies"] = []
         var copies = doc.data().copies
         for(let i = 0; i < copies.length; i++){ 
